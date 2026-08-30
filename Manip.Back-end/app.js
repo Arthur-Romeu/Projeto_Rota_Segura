db8f1ac86c00e11714f331c64fa93d5f058e9cd3
Arthur Romeu <arthurromeu76@gmail.com>
2026-08-30 14:38:05 -0300

    Arquivos faltantes

--- Manip.Back-end/app.js
+++ Manip.Back-end/app.js
@@ -1,5 +1,5 @@
 const sequelize = require('./Modo_Online/models/ConfigBanco')
-require('dotenv').config({ path: ".env" })
+require('dotenv').config({ path: "arquivo.env" })
 
 const express = require('express')
 const cors = require('cors')
